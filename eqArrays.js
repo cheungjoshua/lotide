a34f63e80b05117b4bf7709153b565a5a8a8b01a
Joshua Cheung <contact.cjoshua@gmail.com>
2022-05-03 18:53:55 -0700

    Completed eqArrays function

---
+++ eqArrays.js
@@ -0,0 +1,20 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function (arrA, arrB) {
+  arrA = JSON.stringify(arrA);
+  arrB = JSON.stringify(arrB);
+  if (arrA === arrB) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
